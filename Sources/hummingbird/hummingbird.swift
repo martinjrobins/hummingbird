93c580c396da27a5ebad85a78f60fe572fad1226
Martin Robinson <martinjrobins@gmail.com>
2020-04-24 11:01:04 +0100

    dormond price works

--- Sources/hummingbird/hummingbird.swift
+++ Sources/hummingbird/hummingbird.swift
@@ -10,7 +10,6 @@ protocol ButchersTableau {
   var c: [Scalar] { get }
   var a: [[Scalar]] { get }
   var b_hat: [Scalar] { get }
-  var b_star: [Scalar] { get }
   var b: [Scalar] { get }
   var p: [[Scalar]] { get }
 }
@@ -26,61 +25,60 @@ struct DormondPrice<Scalar: Real & BinaryFloatingPoint>: ButchersTableau {
       Scalar(1.0), Scalar(1.0)]
   let a: [[Scalar]] = 
     [
-      [Scalar(0.0),             Scalar(0.0),            Scalar(0.0),
-        Scalar(0.0),
-        Scalar(0.0)], 
-      [Scalar(1.0/5.0),         Scalar(0.0),            Scalar(0.0),
-        Scalar(0.0),          Scalar(0.0)], 
-      [Scalar(3.0/40.0),        Scalar(9.0/40.0),       Scalar(0.0),
-        Scalar(0.0),          Scalar(0.0)], 
-      [Scalar(44.0/45.0),      Scalar(-56.0/15.0),
-        Scalar(32.0/9.0),       Scalar(0.0),          Scalar(0.0)], 
-      [Scalar(19372.0/6561.0), Scalar(-25360.0/2187.0),
-        Scalar(64448.0/6561.0), Scalar(-212.0/729.0), Scalar(0.0)], 
-      [Scalar(-9017.0/3168.0), Scalar(-355.0/33.0),
-        Scalar(46732.0/5247.0), Scalar(49.0/176.0),
+      [Scalar(0.0), Scalar(0.0), Scalar(0.0), Scalar(0.0), Scalar(0.0)], 
+      [Scalar(1.0/5.0), Scalar(0.0), Scalar(0.0), Scalar(0.0), Scalar(0.0)], 
+      [Scalar(3.0/40.0), Scalar(9.0/40.0), Scalar(0.0), Scalar(0.0), Scalar(0.0)], 
+      [Scalar(44.0/45.0), Scalar(-56.0/15.0), Scalar(32.0/9.0), Scalar(0.0), Scalar(0.0)], 
+      [Scalar(19372.0/6561.0), Scalar(-25360.0/2187.0), Scalar(64448.0/6561.0), 
+        Scalar(-212.0/729.0), Scalar(0.0)], 
+      [Scalar(9017.0/3168.0), Scalar(-355.0/33.0), Scalar(46732.0/5247.0), Scalar(49.0/176.0),
         Scalar(-5103.0/18656.0)],
       [Scalar(35.0/384.0), Scalar(0.0), Scalar(500.0/1113.0), Scalar(125.0/192.0),
         Scalar(-2187.0/6784.0), Scalar(11.0/84.0)],
     ]
+
+
   let b_hat: [Scalar] = 
     [Scalar(35.0/384.0), Scalar(0.0), Scalar(500.0/1113.0), Scalar(125.0/192.0),
-      Scalar(-2187.0/6784.0), Scalar(11.0/84.0)]
+      Scalar(-2187.0/6784.0), Scalar(11.0/84.0), Scalar(0.0)]
   let b: [Scalar] =
     [Scalar(5179.0/57600.0), Scalar(0.0), Scalar(7571.0/16695.0), Scalar(393.0/640.0),
       Scalar(-92097.0/339200.0), Scalar(187.0/2100.0), Scalar(1.0/40.0)]
-  let b_star: [Scalar] =
-    [Scalar(6025192743.0/30085553152.0), Scalar(0.0),
-      Scalar(51252292925.0/65400821598.0),
-      Scalar(-2691868925.0/45128329728.0), Scalar(187940372067.0/1594534317056.0),
-      Scalar(-1776094331.0/19743644256.0), Scalar(11237099.0/235043384.0)]
-
   let p: [[Scalar]] = 
     [
-      [Scalar(1.0), Scalar(-32272833064.0/11282082432.0),
+      [Scalar(1.0), 
+        Scalar(-32272833064.0/11282082432.0),
         Scalar(34969693132.0/11282082432.0),
         Scalar(-13107642775.0/11282082432.0),
         Scalar(157015080.0/11282082432.0)],
       [Scalar(0.0), Scalar(0.0), Scalar(0.0), Scalar(0.0), Scalar(0.0)],
-      [Scalar(0.0), Scalar(1323431896.0*100.0/32700410799.0),
+      [Scalar(0.0), 
+        Scalar(1323431896.0*100.0/32700410799.0),
         Scalar(-2074956840.0*100.0/32700410799.0),
         Scalar(914128567.0*100.0/32700410799.0),
         Scalar(-15701508.0*100.0/32700410799.0)],
-      [Scalar(0.0), Scalar(-889289856.0*25.0/5641041216.0),
+      [Scalar(0.0), 
+        Scalar(-889289856.0*25.0/5641041216.0),
         Scalar(2460397220.0*25.0/5641041216.0),
         Scalar(-1518414297.0*25.0/5641041216.0),
         Scalar(94209048.0*25.0/5641041216.0)],
-      [Scalar(0.0), Scalar(259006536.0*2187.0/199316789632.0),
+      [Scalar(0.0), 
+        Scalar(259006536.0*2187.0/199316789632.0),
         Scalar(-687873124.0*2187.0/199316789632.0),
         Scalar(451824525.0*2187.0/199316789632.0),
         Scalar(-52338360.0*2187.0/199316789632.0)], 
-      [Scalar(0.0), Scalar(-361440756.0*11.0/2467955532.0),
+      [Scalar(0.0), 
+        Scalar(-361440756.0*11.0/2467955532.0),
         Scalar(946554244.0*11.0/2467955532.0),
         Scalar(-661884105.0*11.0/2467955532.0),
         Scalar(106151040.0*11.0/2467955532.0)],
-      [Scalar(0.0), Scalar(44764047.0/29380423.0), Scalar(-127201567/29380423.0), 
-        Scalar(90730570.0/29380423.0), Scalar(-8293050.0/29380423.0)],
+      [Scalar(0.0), 
+        Scalar(44764047.0/29380423.0), 
+        Scalar(-127201567/29380423.0), 
+        Scalar(90730570.0/29380423.0), 
+        Scalar(-8293050.0/29380423.0)],
     ]
+
 }
 
 
@@ -111,63 +109,63 @@ func explicit_runge_kutta<Vector: HumVector, Tableau: ButchersTableau>(tableau: 
     return 0
   }
   var t_n = ts[0]
-  var dydx_n = dydx(y0, t_n)
   var h_n = ts[N - 1] - t_n
-
+  var step_count = 0
+  k[stages - 1] = dydx(y0, t_n)
   while (t_n < ts[N - 1]) { 
     var step_rejected = true 
     while (step_rejected) {
-      // used to store the last dydx eval so can be re-used at the next step
-      var dydx_stored = dydx_n
+      //print("attempting step \(step_count) with it = \(it), h_n = \(h_n)")
 
-      // calculate stages
-      k[0] = h_n * dydx_n
-      var sum_ak: Vector     
+      // reuse last k (we have asserted that the last c value is 1.0)
+      let last_k_store = k[stages - 1]
+      k[0] = k[stages - 1]
       for i in 1..<stages {
-        sum_ak = Vector(repeating: 0)     
+        var sum_ak = Vector(repeating: 0)     
         for j in 0..<i {
           sum_ak += a[i][j] * k[j]
         }
-        // Note: if step is successful, this value at i=stages-1 
-        // will be reused at the start of next step
-        dydx_stored = dydx(y_hat_n + sum_ak, t_n + h_n * c[i])
-        k[i] = h_n * dydx_stored
+        k[i] = dydx(y_hat_n + h_n * sum_ak, t_n + c[i] * h_n)
       }
 
       // calculate final value and error
       var error = Vector(repeating: 0)
-      var y_hat_np1 = y_hat_n
+      var sum_bk = Vector(repeating: 0)
       for i in 0..<stages {
-        y_hat_np1 += b_hat[i] * k[i]
+        sum_bk += b_hat[i] * k[i]
         error += (b_hat[i] - b[i]) * k[i]
       }
+      let y_hat_np1 = y_hat_n + h_n * sum_bk
 
-      let E_hp1 = error.inf_norm()
+      // check if step is successful, i.e error is below set tolerance
+      let E_hp1 = (h_n * error).inf_norm()
       if (E_hp1 < tol) {
         // if moved over any requested times then interpolate their values
         let t_np1 = t_n + h_n
         while (it < ts.count && t_np1 >= ts[it]) {
           let sigma = (ts[it] - t_n) / h_n
-          var sigma_i = sigma
-          var y_sigma = y_hat_n
-          for i in 1...stages {
-            sigma_i = sigma 
-            var b_i = sigma_i * p[i][0]
+          var Phi = Vector(repeating: 0)
+          for i in 0..<stages {
+            var term = sigma 
+            var b_i = term * p[i][0]
             for j in 1..<dense_order {
-              sigma_i *= sigma
-              b_i += sigma_i * p[i][j]
+              term *= sigma
+              b_i += term * p[i][j]
             }
-            y_sigma += b_i * k[i]
+            Phi += b_i * k[i]
           }
-          ys[it] = y_sigma
+          ys[it] = y_hat_n + h_n * Phi 
           it += 1
         }
 
         // move to next step
         step_rejected = false
-        dydx_n = dydx_stored
         y_hat_n = y_hat_np1
         t_n = t_np1
+        step_count += 1
+      } else {
+        // failed step, reset last k back to stored value
+        k[stages - 1] = last_k_store
       }
       
       // adapt step size
